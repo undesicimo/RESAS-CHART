078065403094e757120b32bb9d04157379362856
undesicimo <105863111+undesicimo@users.noreply.github.com>
2024-04-11 14:48:25 +0900

    Update: higher level components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+'use client';
+
+import Charts from '@/components/Chart';
+import PrefectureFilter from '@/components/Prefecturefilter';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: Infinity,
+		},
+	},
+});
+
 export default function Home() {
-	return <main>Hello World</main>;
+	const [selectedPrefectures, setSelectedPrefectures] = useState<Array<number>>(
+		[]
+	);
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<main>
+				<PrefectureFilter
+					setSelectedPrefectures={setSelectedPrefectures}
+					selectedPrefectures={selectedPrefectures}
+				/>
+				<Charts selectedPrefectures={selectedPrefectures} />
+			</main>
+		</QueryClientProvider>
+	);
 }
