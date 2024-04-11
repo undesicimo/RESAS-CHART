f3e659efe58729edc5f6b5b18884198c2718068a
undesicimo <105863111+undesicimo@users.noreply.github.com>
2024-04-11 14:47:43 +0900

    Feat: Chart components

---
+++ components/Chart/hooks/useChart.ts
@@ -0,0 +1,45 @@
+import { usePrefectureOptions } from '@/components/Prefecturefilter/hooks/usePrefectureOptions';
+import { getPopulationComposition } from '@/services/population/getPopulationComposition';
+import { useQueries } from '@tanstack/react-query';
+
+type Params = {
+	selectedPrefectures: Array<number>;
+};
+
+export function useChart({ selectedPrefectures }: Params) {
+	const { data: prefectureOptions } = usePrefectureOptions();
+
+	const results = useQueries({
+		queries: selectedPrefectures.map(prefCode => {
+			return {
+				queryKey: [prefCode],
+				queryFn: async () => {
+					const res = await getPopulationComposition({ prefCode });
+					const prefectureOption = prefectureOptions?.find(
+						pref => pref.prefecture.prefCode === prefCode
+					);
+					const totalPopulation = res.result.data.find(
+						f => f.label === '総人口'
+					);
+
+					return {
+						id: prefectureOption?.prefecture.prefName,
+						color: prefectureOption?.colorStroke,
+						data: totalPopulation?.data.map(d => ({
+							x: d.year,
+							y: d.value,
+						})),
+					};
+				},
+			};
+		}),
+		combine: results => {
+			return {
+				data: results.map(result => result.data),
+				pending: results.some(result => result.isPending),
+			};
+		},
+	});
+
+	return results;
+}
---
+++ components/Chart/index.tsx
@@ -0,0 +1,94 @@
+import { useChart } from './hooks/useChart';
+import { ResponsiveLine, Serie } from '@nivo/line';
+import './index.css';
+
+interface ChartsProps {
+	selectedPrefectures: Array<number>;
+}
+
+export default function Charts({ selectedPrefectures }: ChartsProps) {
+	const { data, pending } = useChart({ selectedPrefectures });
+
+	if (pending) {
+		return (
+			<figure className='Skeleton'>
+				<p className='Skeleton-text'>{'データ取得中...'}</p>
+			</figure>
+		);
+	}
+
+	return (
+		<section className='Chart'>
+			<figure className='Chart-wrapper'>
+				<span className='Chart-YAxisLabel'>{'人口数'}</span>
+				<div className='LineChart-wrapper'>
+					<LineChart data={data as readonly Serie[]} />
+				</div>
+				<span className='Chart-XAxisLabel'>{'年度'}</span>
+			</figure>
+		</section>
+	);
+}
+
+const LineChart = ({ data }: { data: readonly Serie[] }) => (
+	<ResponsiveLine
+		data={data}
+		colors={{ datum: 'color' }}
+		margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+		xScale={{ type: 'point' }}
+		yScale={{
+			type: 'linear',
+			min: 'auto',
+			max: 'auto',
+		}}
+		yFormat={v => {
+			return `${v.toLocaleString()}人`;
+		}}
+		xFormat={v => {
+			return `${v}年`;
+		}}
+		axisBottom={{
+			legendOffset: 40,
+			legendPosition: 'end',
+		}}
+		axisLeft={{
+			tickSize: 5,
+			tickPadding: 5,
+			tickRotation: 0,
+			legendOffset: -40,
+			legendPosition: 'end',
+			truncateTickAt: 0,
+		}}
+		enablePoints={false}
+		enableGridX={false}
+		enableGridY={false}
+		enableCrosshair={false}
+		useMesh={true}
+		legends={[
+			{
+				anchor: 'bottom-right',
+				direction: 'column',
+				justify: false,
+				translateX: 100,
+				translateY: 0,
+				itemsSpacing: 0,
+				itemDirection: 'left-to-right',
+				itemWidth: 80,
+				itemHeight: 20,
+				itemOpacity: 0.75,
+				symbolSize: 12,
+				symbolShape: 'square',
+				symbolBorderColor: 'rgba(0, 0, 0, .5)',
+				effects: [
+					{
+						on: 'hover',
+						style: {
+							itemBackground: 'rgba(0, 0, 0, .03)',
+							itemOpacity: 1,
+						},
+					},
+				],
+			},
+		]}
+	/>
+);
