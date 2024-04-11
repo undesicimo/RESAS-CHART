ddfe6d40ff75a18fca3aa02929ad6d9ad730fc25
undesicimo <105863111+undesicimo@users.noreply.github.com>
2024-04-11 09:30:09 +0900

    Feat: env variables

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+namespace NodeJS {
+	interface ProcessEnv {
+		APP_BASE_URL: string;
+		RESAS_BASE_URL: string;
+		RESAS_API_KEY: string;
+	}
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	env: {
+		APP_BASE_URL: process.env.APP_BASE_URL,
+	},
+};
 
 export default nextConfig;
